caf056ccd04b29ca94ccccb2b6f76c96eee577f2
Mathijs Bakker <mathijs_bakker@me.com>
2018-03-27 19:55:14 +0200

    Dynamite and PlayerActionHandler added. Introduction of IDamagable for all creeps and world objects who can be destroyed by the player.

---
+++ Assets/Code/Player/PlayerActionHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Code
+{
+    public class PlayerActionHandler : ITickable
+    {
+        private readonly Player _player;
+        private readonly PlayerInputState _inputState;
+        
+        public PlayerActionHandler(
+            Player player,
+            PlayerInputState playerInputState)
+        {
+            _player = player;
+            _inputState = playerInputState;
+        }
+        
+        public void Tick()
+        {
+            if (_player.IsDead) return;
+
+            if (_inputState.IsMovingDown && _player.IsGrounded)
+            {
+                // Place Dynamite
+                Debug.Log("Dynamite placed");
+            }
+        }
+    }
+}
---
+++ Assets/Code/World/IDamageable.cs
@@ -0,0 +1,6 @@
+﻿namespace Code
+{
+    public interface IDamageable
+    {
+    }
+}
