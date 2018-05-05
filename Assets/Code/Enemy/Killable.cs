058baa354fbf2c99acc71011b88c21a421e6cfd8
Mathijs-Bakker <mathijs_bakker@me.com>
2018-05-05 16:21:38 +0200

    [file] Moved Killable base class to Enemy directory

--- Assets/Code/Enemy/Killable.cs
+++ Assets/Code/Enemy/Killable.cs
@@ -1,6 +1,8 @@
-﻿namespace Code
+﻿using UnityEngine;
+
+namespace Code
 {
-    public abstract class Killable
+    public abstract class Killable : MonoBehaviour
     {
         public abstract void ReceiveDamage(int damage);
 
