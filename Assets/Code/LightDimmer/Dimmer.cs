d569866f23201cadf974289ee87e0673243c0fcd
Mathijs-Bakker <mathijs_bakker@me.com>
2018-05-07 00:33:03 +0200

    [new] LightDimmer Class

---
+++ Assets/Code/LightDimmer/Dimmer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    public class Dimmer : MonoBehaviour 
+    {
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            var spriteRenderer = other.gameObject.GetComponent<SpriteRenderer>();
+            if (spriteRenderer == null) return;
+            
+            spriteRenderer.color = Color.black;
+            
+        }
+    }
+}
