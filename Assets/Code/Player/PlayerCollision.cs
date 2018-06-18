b8ebb3dae28f2310dcf0d84f7be17bba46e4d41a
Mathijs Bakker <mathijs_bakker@me.com>
2018-06-18 21:11:01 +0200

    [add] PlayerCollision class
    Collision detection has been moved from PlayerFacade to it's own class

---
+++ Assets/Code/Player/PlayerCollision.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace Code
+{
+    public class PlayerCollision : MonoBehaviour
+    {
+        private PlayerGroundedHandler _playerGroundedHandler;
+        private PlayerFacade _playerFacade;
+
+        [Inject]
+        public void Construct(
+            PlayerGroundedHandler playerGroundedHandler,
+            PlayerFacade playerFacade)
+        {
+            _playerGroundedHandler = playerGroundedHandler;
+            _playerFacade = playerFacade;
+        }
+
+        
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.collider.CompareTag("Floor")) _playerGroundedHandler.PlayerHitFloor(other);
+
+            if (other.collider.CompareTag("Miner"))
+            {
+                _playerFacade.HasWon = true;
+                Debug.Log("Win!");
+            }
+        }
+    }
+}
