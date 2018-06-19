524839bab6dac80463aca41aa45b99351c44a4de
Mathijs Bakker <mathijs_bakker@me.com>
2018-06-19 13:26:05 +0200

    [add] New DynamiteCounter class and UI sprite

---
+++ Assets/Code/UI/DynamiteCounter.cs
@@ -0,0 +1,20 @@
+﻿namespace Code
+{
+    public class DynamiteCounter
+    {
+        private const int MaxDynamites = 6;
+
+        private int _dynamitesLeft;
+
+        private void ResetDynamiteCounter()
+        {
+            _dynamitesLeft = MaxDynamites;
+        }
+        
+        private void RemoveDynamite()
+        {
+            if (_dynamitesLeft <=0) return;
+            _dynamitesLeft -= 1;
+        }
+    }
+}
