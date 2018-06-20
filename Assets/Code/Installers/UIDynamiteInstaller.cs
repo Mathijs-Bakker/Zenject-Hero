b7fc48b2463fcaa84baebd684c7bac06be11a8dd
Mathijs Bakker <mathijs_bakker@me.com>
2018-06-20 16:19:21 +0200

    [dev] Change UIDynamite from Scene- to ObjectContext

--- Assets/Code/Installers/UIDynamiteInstaller.cs
+++ Assets/Code/Installers/UIDynamiteInstaller.cs
@@ -13,20 +13,7 @@ namespace Code
             Container.BindInterfacesTo<UIDynamiteManager>().AsSingle();
             Container.BindFactory<UIDynamite, UIDynamite.Factory>()
                 .FromComponentInNewPrefab(UIDynamitePrefab)
-                .UnderTransform(ParentGetter);
-        }
-
-        private string parentGO = "Dynamite HUD";
-        private Transform ParentGetter(InjectContext arg)
-        {
-            var go = GameObject.Find(parentGO);
-
-            if (go != null) return go.transform;
-
-            Debug.LogError("::: GameObject not found! :::\n" +
-                           "Failed to find: " + parentGO);
-            
-            return transform;
+                .UnderTransform(transform);
         }
     }
 }
